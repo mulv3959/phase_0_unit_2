5a95c4ca564c949164a39178be08c8137649aa01
mulv3959 <mulv3959@users.noreply.github.com>
2014-04-28 13:21:25 -0500

    Update my_solution.rb

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -8,9 +8,13 @@
 # 1. Pseudocode
 
 # What is the input?
+# input a integer
 # What is the output? (i.e. What should the code return?)
+# returns a string with a comma separating every three characters
 # What are the steps needed to solve the problem?
-
+# Well first I believe we need to turn it into a string.
+# After it is in a string I have to find a pattern that allows me to tell the computer 
+# to insert a comma in the correct spot (maybe using the split method?)
 
 # 2. Initial Solution
 
